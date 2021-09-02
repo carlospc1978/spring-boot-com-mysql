66adbadfc99bd3071addc5200238082428e84695
Carlos Pijanowski <carlos.pijanowski@gmail.com>
2021-09-02 20:18:11 -0300

    note Acer

--- src/main/java/br/com/seteideias/springbootcommysql/SpringBootComMysqlApplication.java
+++ src/main/java/br/com/seteideias/springbootcommysql/SpringBootComMysqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootComMysqlApplication {
 
 	// TODO CONFIGURANDO O AMBIENTE
+	//note acer
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootComMysqlApplication.class, args);
 	}
